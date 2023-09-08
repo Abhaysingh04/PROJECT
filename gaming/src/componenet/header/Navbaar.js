183b88aa1fa7e6bdc3afec9a7448821fef1e6c59
abhaysingh04 <abhaysingh14310@gmail.com>
2023-09-08 20:03:48 +0530

    creat react app

---
+++ gaming/src/componenet/header/Navbaar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./navbaar.css";
+
+
+function Navbaar() {
+  return (
+    <header>
+      <nav>
+        <div classname="left">
+          <div classname="navlogo">
+            <img src="./images/amazon.png" alt="not working " />
+          </div>
+          <div classname="nav_searchbaar">
+            <input type="text" name="" id="" />
+              
+          </div>
+        </div>
+        <div classname="right">
+
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbaar
